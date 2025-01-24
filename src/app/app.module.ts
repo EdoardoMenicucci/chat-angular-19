3c0998a4fc5afe6a67784af2f9587d3c0267504b
EdoardoMenicucci <edoardo.menicucci@libero.it>
2025-01-24 14:42:07 +0100

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MarkdownModule],
   providers: [],
 })
 export class AppModule {}
